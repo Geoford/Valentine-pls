f06cf58a4a695c2312167efa92f42104c85fc806
Geoford <christianleemontero@gmail.com>
2025-02-02 22:44:54 +0800

    Initial commit

---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer>
+            {/* <p>© 2021 Valentine</p> */}
+            <p>&copy; {new Date().getFullYear()} Pls mahalin moko hehe </p>
+        </footer>
+    );
+}
+export default Footer;
